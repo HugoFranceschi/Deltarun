95ee39c4bf9c2709679ecadf245c79ab06a17b92
HugoFranceschi <franceschi.hugo@yahoo.com>
2025-11-06 15:20:57 +0100

    GERSON

--- mainGerson.js
+++ mainGerson.js
@@ -1,8 +1,12 @@
+const musique = new Audio(
+  "musique/Toby Fox - DELTARUNE Chapters 3+4 OST - 58 Hammer of Justice.flac"
+);
+
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
 ctx.imageSmoothingEnabled = false;
 ctx.fillStyle = "black";
-ctx.fillRect(0, 0, 0, 0); // le canva fais 290 pour x et 145 pour y
+ctx.fillRect(0, 0, 0, 0);
 ctx.fillStyle = "red";
 
 const DEBUG = true;
@@ -11,36 +15,38 @@ ctx.strokeStyle = "green";
 ctx.strokeRect(90, 35, 105, 75);
 ctx.lineWidth = 5;
 
-let x = 174;
-let y = 60;
+let x = 331;
+let y = 240;
 
-let dx = 141;
-let dy = 57;
-var image = document.getElementById("SOUL");
+let dx = 325;
+let dy = 230;
+var image = Object.assign(new Image(), { src: "assets/SOUL_red.png" });
 
-var Susie = new Image();
-Susie.src = "./assets/Susie.png";
-let sx = 10;
-let sy = 46;
+var Susie = Object.assign(new Image(), { src: "./assets/Susie.png" });
+let sx = 50;
+let sy = 186;
 
-var GersonBody = new Image();
-GersonBody.src = "./assets/Gerson.png";
-let GBx = 220;
-let GBy = 50;
+var GersonBody = Object.assign(new Image(), { src: "assets/Gerson.png" });
+let GBx = 465;
+let GBy = 198;
 
-var GersonHear = new Image();
-GersonHear.src = "./assets/Gerson.png";
-let GHx = 230;
-let GHy = 40;
+var GersonHear = Object.assign(new Image(), { src: "assets/Gerson.png" });
+let GHx = 495;
+let GHy = 175;
+
+var flecheBas = Object.assign(new Image(), { src: "assets/fleche.png" });
+let fbx = 0;
+let fby = 0;
 
 ctx.drawImage(image, dx, dy, 10, 10);
 
 cycle = 0;
 
 setInterval(() => {
+  musique.play();
   ctx.fillStyle = "black";
 
-  ctx.fillRect(0, 0, 500, 500);
+  ctx.fillRect(0, 0, 640, 480);
   ctx.fillStyle = "red";
   ctx.drawImage(image, dx, dy);
 
@@ -57,11 +63,11 @@ setInterval(() => {
   );
   ctx.drawImage(
     GersonHear,
-    1525 + 56 * (Math.floor(cycle / 7) % 5),
+    1525 + 56 * (Math.floor(cycle / 10) % 5),
     187,
     55,
     33,
-    GHx,
+    GHx + 7 * Math.sin(cycle / 20),
     GHy,
     110,
     66
@@ -72,14 +78,25 @@ setInterval(() => {
     181,
     67,
     39,
-    GBx,
+    GBx + 7 * Math.sin(cycle / 20),
     GBy,
-    114,
-    78
+    67 * 2,
+    39 * 2
+  );
+  ctx.drawImage(
+    flecheBas,
+    0 * (Math.floor(cycle / 1) % 1),
+    0,
+    25,
+    24,
+    fbx,
+    fby,
+    25,
+    24
   );
 
   ctx.strokeStyle = "green";
-  ctx.strokeRect(246, 95, 150, 150);
+  ctx.strokeRect(246, 155, 170, 170);
 
   if (DEBUG) ctx.fillRect(x, y, 4, 4);
   cycle++;
@@ -89,25 +106,25 @@ document.addEventListener("keydown", (event) => {
   console.log("position x " + x);
   console.log("position y " + y);
   if (event.key == "ArrowDown") {
-    if (y < 90) {
+    if (y < 315) {
       y = y + 5;
       dy = dy + 5;
     }
   }
   if (event.key == "ArrowUp") {
-    if (y > 30) {
+    if (y > 170) {
       y = y - 5;
       dy = dy - 5;
     }
   }
   if (event.key == "ArrowRight") {
-    if (x < 170) {
+    if (x < 401) {
       x = x + 5;
       dx = dx + 5;
     }
   }
   if (event.key == "ArrowLeft") {
-    if (x > 115) {
+    if (x > 256) {
       x = x - 5;
       dx = dx - 5;
     }
